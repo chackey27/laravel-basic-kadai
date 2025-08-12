bdd87cb34e1b7c915ff0719a22ec5a3b91e69e9e
chackey27 <f.masahirox2@gmail.com>
2025-08-12 13:49:53 +0900

    kadaiteisyutu

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PostController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,3 +21,5 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/{id}', [PostController::class, 'show']);
